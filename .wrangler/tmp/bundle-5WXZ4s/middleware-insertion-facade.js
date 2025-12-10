fbe3d29289cd074cfe8385cd5470d1e6f0f22b65
Omefans <uu12244644@gmail.com>
2025-12-10 17:59:29 +0100

    d

---
+++ .wrangler/tmp/bundle-5WXZ4s/middleware-insertion-facade.js
@@ -0,0 +1,10 @@
+				import worker, * as OTHER_EXPORTS from "C:\\Users\\S\\Desktop\\website2\\website2\\website2\\src\\worker.ts";
+				import * as __MIDDLEWARE_0__ from "C:\\Users\\S\\Desktop\\website2\\website2\\website2\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+
+				export * from "C:\\Users\\S\\Desktop\\website2\\website2\\website2\\src\\worker.ts";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default
+				]
+				export default worker;
